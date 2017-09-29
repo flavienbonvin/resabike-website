355fcad9420bdfc6e1ff7ea1b03f17568f78a7ce
max529 <maxime.betrisey@hotmail.com>
2017-09-29 13:04:24 +0200

    view add line

---
+++ public/js/js.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    if($("#errorBox").length>0){
+        $('#errorBox').modal('show')
+    }
+    else if($("#msgBox").length>0){
+        $('#msgBox').modal('show')
+    }
+})
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const adminApiConn = require('../modules/adminApiConn');
 
 /* GET home page. */
-router.get('/addLine', function(req, res, next) {
-  res.render('addLine', { title: 'Express' });
+router.get('/addLine', function (req, res, next) {
+  res.render('addLine', { title: 'Express',msg:'test' });
 });
+router.post('/addLine', function (req, res, next) {
+  console.log(req.body)
+  adminApiConn.getStopsForLine(req.body.depart, req.body.arrivee).then((stops) => {
+    res.render('addLine', { title: 'Express', stops: stops });
+  }).catch((error) => {
+    res.render('addLine', { title: 'Express', error: error });
+  })
+});
+router.post('/addLine/add', function (req, res, next) {
+  adminApiConn.getStopsForLine(req.body.depart, req.body.arrivee).then((stops) => {
+    adminApiConn.insertLineInDb().then((res) => {
+      res.render('addLine', { title: 'Add Line', msg: 'Added' })
+    })
+  }).catch((error) => {
+    res.render('addLine', { title: 'Express', error: error });
+  })
+})
 
 module.exports = router;
