3f4cbc5c5e69dba521ba4987a04214d1b39c5ff7
max529 <maxime.betrisey@hotmail.com>
2017-10-05 10:37:11 +0200

    update Zoen

--- modules/admin/zoneManagement.js
+++ modules/admin/zoneManagement.js
@@ -31,7 +31,6 @@ module.exports = {
 
     deleteZone(body) {
         return new Promise((resolve, reject) => {
-            console.log('ifno')
             database.Zone.destroy({
                 where: {
                     id: body.idToDel
@@ -41,6 +40,44 @@ module.exports = {
             }).catch((error) => {
                 reject(error);
             })
+        })
+    },
+    updateZone(body) {
+        return new Promise((resolve, reject) => {
+            database.Zone.update({
+                name: body.newName
+            }, {
+                    where: {
+                        id: body.idToUpdate
+                    }
+                }).then((zoneTemp) => {
+                    resolve();
+                }).catch((error) => {
+                    reject(error);
+                })
+        })
+    },
+    listWithDetails() {
+        var res = [];
+        database.Zone.findAll().then((list) => {
+            for (var i = 0; i < list.length; i++) {
+                database.Line.findAll({ where: { idZone: list[i].id } }).then((listStation) =>{
+                    var temp = {
+                        id:0,
+                        name:'zone',
+                        stations : [
+                            {
+                                id:0,
+                                name:'line1'
+                            },
+                            {
+                                id:1,
+                                name:'line2'
+                            }
+                        ]
+                    }
+                })
+            }
         })
     }
 }
--- public/js/admin/addZone.js
+++ public/js/admin/addZone.js
@@ -9,7 +9,30 @@ function loadZone(){
         success: (res) => {
             var txt = '';
             for (var i in res) {
-                txt += '<div class="item"><div class="right floated content"><div class="ui button">Update</div><div class="ui button" onclick="deleteZone('+res[i].id+')">Delete</div></div><div class="content">'+res[i].name+'</div></div>'
+                txt += '<div class="item">'+
+                            '<div class="right floated content">'+
+                                '<div class="ui button" onclick="updateZone('+res[i].id+',this)">Update</div>'+
+                                '<div class="ui button" onclick="deleteZone('+res[i].id+')">Delete</div>'+
+                            '</div>'+
+                            '<div class="content">'+
+                                '<div class="ui form">'+
+                                    '<div class="two fields">'+
+                                        '<div class="field">'+
+                                            '<input id="updateName'+res[i].id+'" type="text">'+
+                                        '</div>'+
+                                        '<div class="field">'+
+                                            '<div class="ui button" onclick="saveUpdate()">Save</div>'+
+                                            '<div class="ui button" onclick="cancelUpdate()">Cancel</div>'+
+                                        '</div>'+
+                                    '</div>'+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="content">'+
+                                '<span>'+res[i].name+'</span>'+
+                            '</div>'+
+                        '</div>'
+
+
             }
             document.getElementById("pastModif").innerHTML = txt;
         }
@@ -20,6 +43,38 @@ function deleteZone(id){
         url: '/admin/zone/delete',
         type: 'POST',
         data: 'idToDel='+id,
+        success: (res) => {
+            loadZone();
+        }
+    })
+}
+var savedName = "";
+var savedId = -1; 
+var savedRoot = null;
+function updateZone(id,t){
+    var root = t.parentNode.parentNode;
+    savedRoot = root;
+    savedName = root.children[2].children[0].innerHTML;
+    savedId = id;
+
+    document.getElementById('updateName'+id).value = savedName;
+
+    root.children[0].style.display='none';
+    root.children[2].style.display='none';
+    root.children[1].style.display='block';
+}
+
+function cancelUpdate(){
+    document.getElementById('updateName'+savedId).value = savedName;
+    savedRoot.children[0].style.display='block';
+    savedRoot.children[2].style.display='block';
+    savedRoot.children[1].style.display='none';
+}
+function saveUpdate(){
+    $.ajax({
+        url: '/admin/zone/update',
+        type: 'POST',
+        data: 'idToUpdate='+savedId+"&newName="+document.getElementById('updateName'+savedId).value,
         success: (res) => {
             loadZone();
         }
