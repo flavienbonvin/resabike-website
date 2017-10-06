5e9c4ee2232cb52a8e10e4b0cf1669877e89b2bd
flavien <bonvin.flavien@gmail.com>
2017-10-06 16:21:53 +0200

    connection fetching, default method add date options

--- test.js
+++ test.js
@@ -2,13 +2,23 @@ var zonem = require('./modules/admin/zoneManagement');
 var db = require('./modules/database');
 var zone = require('./modules/admin/zoneManagement');
 var render = require('./modules/renderAddon');
+var connection = require('./modules/client/connectionManagement');
 var fs = require('fs');
 var opn = require('opn');
 
-zone.listWithDetails().then((res) => {
-    var obj = JSON.parse(JSON.stringify(res));
-    var op = render.readableObject(obj);
-    fs.writeFileSync('index.html',op);
-    opn('./index.html');
-    db.close();
+// zone.listWithDetails().then((res) => {
+//     var obj = JSON.parse(JSON.stringify(res));
+//     var op = render.readableObject(obj);
+//     fs.writeFileSync('index.html',op);
+//     opn('./index.html');
+//     db.close();
+// })
+
+var body = {
+    from: "Vex, poste",
+    to: "Euseigne, Ritzé"
+}
+
+connection.getConnectionForTrip(body).then((res) => {
+    console.log(res)
 })
