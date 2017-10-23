ea6e575104fae55f01373d99928f7ded77d86d97
flavienbonvin <bonvin.flavien@gmail.com>
2017-10-23 17:45:18 +0200

    updte button

--- public/js/admin/js.js
+++ public/js/admin/js.js
@@ -1,3 +1,11 @@
 $(document).ready(() => {
     $('select.dropdown').dropdown();
+    updateMenu();
 })
+
+
+function updateMenu(){
+    $('#Adminmenu .item').removeClass("active item");
+    
+    document.getElementById('Adminmenu').children[noOnglet].setAttribute('class', 'active item');
+}
