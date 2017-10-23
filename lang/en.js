da1a717611764c4f2970e3f02489e62eca02bcba
flavienbonvin <bonvin.flavien@gmail.com>
2017-10-23 15:03:24 +0200

    translation

--- lang/en.js
+++ lang/en.js
@@ -9,7 +9,102 @@ module.exports = {
     optionStart: 'Place of departure',
     labelEnd: 'Arrival',
     optionEnd: 'Arrival point',
-    buttonCreateLine: 'Create',
+    buttonCreate: 'Create',
     labelZone: 'Zone',
-    buttonValidate: 'Validate'
+    buttonValidate: 'Validate',
+
+    //AddUser
+    titleAddUser: 'Add a new user',
+    labelRole: 'Role of the new user',
+    optionRole: 'Role',
+    optionBusDriver: 'Bus driver',
+    optionZoneAdmin: 'Zone admin',
+    optionSysAdmin: 'System admin',
+    labelUsername: 'Username',
+    labelPassword: 'Password',
+    labelPasswordConfirm: 'Confirm the password',
+    labelEmail: 'Email',
+    labelSelectZone: 'Select the zone',
+    labelResetPassword: 'Reset the password at first connect',
+
+    //Index
+    titleZoneList: 'List of zones',
+    buttonDelete: 'Delete',
+    labelLine: 'Line',
+
+    //Login
+    titleLogin: 'Login',
+    labelConnect: 'Connect',
+
+    //UserManagement
+    titleUserManagement: 'Users management',
+    //TODO: CHange text in javascipt
+
+    //Remorques
+    titleTrailerManagement: 'Trailer management',
+    //TODO: finish once the page is finished
+
+    //Zone
+    titleAddNewZone: 'Add a new zone',
+    labelZoneName: 'Name of the zone',
+    titleEditZone: 'Edit zones ',
+    //TODO: change text in javascript
+
+    //Menu title
+    home: 'Home',
+    addLineMenu: 'Add a line',
+    addUserMenu: 'Add a user',
+    manageZoneMenu: 'Manage zones',
+    manageUserMenu: 'Manage users',
+    manageTrailerMenu: 'Manage trailers',
+
+
+    /*
+        CLIENT
+    */
+    //Index
+    titleReservation: 'Reservation',
+    labelDateOfReservation: 'Date of the reservation',
+    labelNameForname: 'Your name, forename',
+    labelNumberOfBikes: 'Number of bikes',
+    labelZoneChoice: 'Choose your zone',
+    labelDepartureChoice: 'Choose your departure',
+    labelDestinationChoice: 'Choose your destination',
+    labelFrom: 'From',
+    labelTo: 'to',
+    labelFor: 'For',
+    labelForBikeNumber: 'bike(s), booked by',
+    buttonNewBooking: 'New booking',
+    buttonBook: 'Book',
+    minutes: 'minuts',
+    administratorNeeded: 'Administrator confirmation needed',
+
+
+    //CancelBooking
+    titleConfirmBookCancel: 'Deletion confirmation',
+    tableTitleSchedule: 'Schedule',
+    tableTitleName: 'Name',
+    tableTitleStartStation: 'Start station',
+    tableTitleEndStation: 'End station',
+    buttonConfirmDelete: 'Confirm the removal',
+
+
+    /*
+        DRIVER
+    */
+    //Index
+    titleSelectLine: 'Select the line',
+    tableTitleStartHour: 'Start hour',
+    tableTitleTrailer: 'Trailer',
+    tableTitleAction: 'Action',
+    labelYes: 'Yes',
+    labelNo: 'No',
+    buttonDetails: 'Details',
+
+
+    //Details
+    buttonReturn: 'Return',
+    titleTripsWithBikes: 'Trips with bikes',
+    tableTitleEnterStation: 'Enter station',
+    tableTitleExitStation: 'Exit station',
 }
--- lang/fr.js
+++ lang/fr.js
@@ -9,7 +9,100 @@ module.exports = {
     optionStart: 'Lieu de départ',
     labelEnd: 'Arrivée',
     optionEnd: 'Lieu d\'arrivée',
-    buttonCreateLine: 'Créer',
+    buttonCreate: 'Créer',
     labelZone: 'Zone',
-    buttonValidate: 'Valider'
+    buttonValidate: 'Valider',
+
+    //AddUser
+    titleAddUser: 'Ajout d\'un utilisateur',
+    labelRole: 'Role du nouvel utilisateur',
+    optionRole: 'Role',
+    optionBusDriver: 'Chauffeur de bus',
+    optionZoneAdmin: 'Administrateur de zone',
+    optionSysAdmin: 'Administrateur system',
+    labelUsername: 'Nom d\'utilisateur',
+    labelPassword: 'Mot de passe',
+    labelPasswordConfirm: 'Confirmer le mot de passe',
+    labelEmail: 'Email',
+    labelSelectZone: 'Selectionnez la zone ',
+    labelResetPassword: 'Changer de mot de passe lors de la première connexion',
+
+    //Index
+    titleZoneList: 'Liste des zones',
+    buttonDelete: 'Supprimer',
+    labelLine: 'Ligne',
+
+    //Login
+    titleLogin: 'Login',
+    labelConnect: 'Connecter',
+
+    //UserManagement
+    titleUserManagement: 'Gestion des utilisateurs',
+    //TODO: CHange text in javascipt
+
+    //Remorques
+    titleTrailerManagement: 'Gestion des remorques',
+    //TODO: finish once the page is finished
+
+    //Zone
+    titleAddNewZone: 'Ajout d\'une zone',
+    labelZoneName: 'Nom de la zone',
+    titleEditZone: 'Modification des zones',
+    //TODO: change text in javascript
+
+    //Menu title
+    home: 'Acceuil',
+    addLineMenu: 'Ajouter une ligne',
+    addUserMenu: 'Ajouter un utilisateur',
+    manageZoneMenu: 'Gestion des zones',
+    manageUserMenu: 'Gestion des utilisateurs',
+    manageTrailerMenu: 'Gestion des remorques',
+
+    /*
+        CLIENT
+    */
+    //Index
+    titleReservation: 'Reservation',
+    labelDateOfReservation: 'Date de la réservation',
+    labelNameForname: 'Votre nom - prénom', 
+    labelNumberOfBikes: 'Nombre de vélo',
+    labelZoneChoice: 'Choisissez votre zone',
+    labelDepartureChoice: 'Choisissez votre départ',
+    labelDestinationChoice: 'Choisissez votre destination',
+    labelFrom: 'De',
+    labelTo: 'à',
+    labelFor: 'Pour',
+    labelForBikeNumber: 'vélo(s) reservé par',
+    buttonNewBooking: 'Nouvelle reservation',
+    buttonBook: 'Réserver',
+    minutes: 'minutes',
+    administratorNeeded: 'Confirmation de l\'administrateur necessaire',
+
+    //CancelBooking
+    titleConfirmBookCancel: 'Confirmation de suppression',
+    tableTitleSchedule: 'Horaire',
+    tableTitleName: 'Nom',
+    tableTitleStartStation: 'Station de départ',
+    tableTitleEndStation: 'Station d\'arrivée',
+    buttonConfirmDelete: 'Confirmer la supprssion',
+
+
+    /*
+        DRIVER
+    */
+    //Index
+    titleSelectLine: 'Selection de la ligne',
+    tableTitleStartHour: 'Heure de départ',
+    tableTitleTrailer: 'Remorque',
+    tableTitleAction: 'Action',
+    labelYes: 'Oui',
+    labelNo: 'Non',
+    buttonDetails: 'Détails',
+
+
+    //Details
+    buttonReturn: 'Retour',
+    titleTripsWithBikes: 'Voyages ayant des vélos',
+    tableTitleEnterStation: 'Station d\'entrée',
+    tableTitleExitStation: 'Station de sortie',
 }
