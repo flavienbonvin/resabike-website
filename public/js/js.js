5f9be090a8157edc84fbf434491e8ddb5c8149f0
max529 <maxime.betrisey@hotmail.com>
2017-11-02 09:08:21 +0100

    ajout de la traduction du JS

--- public/js/js.js
+++ public/js/js.js
@@ -1,21 +1,35 @@
-$(document).ready(function(){
-    if($("#errorBox").length>0){
+$(document).ready(function () {
+    if ($("#errorBox").length > 0) {
         $('#errorBox').modal('show')
     }
-    else if($("#msgBox").length>0){
+    else if ($("#msgBox").length > 0) {
         $('#msgBox').modal('show')
     }
     $('#languageDropdown').val(langUsed);
 })
 
-$('#langSwitch').click(function(v){
+$('#langSwitch').click(function (v) {
     console.log(v.target.value);
 })
 
-function changeLanguage(lang){
+function changeLanguage(lang) {
     var loc = window.location.href;
     var locSplit = loc.split('/');
     locSplit[3] = lang.value;
     loc = locSplit.join('/');
     window.location = loc;
+}
+
+function getTxt(value) {
+    var lang = window.location.href.split('/');
+    lang = lang[3];
+    console.log(lang);
+    $.ajax({
+        url: '/services/traduction?label=' + value+'&lang='+lang,
+        type: 'GET',
+        success: function (res) {
+            console.log(res);
+            return res
+        }
+    })
 }
