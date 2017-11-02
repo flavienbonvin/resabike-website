e50e057cda7c806ce9a3e75b420403c25fa3c300
max529 <maxime.betrisey@hotmail.com>
2017-11-02 09:20:13 +0100

    yxcca

--- services/traduction.js
+++ services/traduction.js
@@ -1,6 +1,9 @@
  module.exports = function(query,session) {
-    var label = query.label;
-    var lang = query.lang;
-    var fichier = require('../lang/'+lang);
-    return fichier[label];
+    return new Promise((resolve,reject) => {
+        var label = query.label;
+        var lang = query.lang;
+        var fichier = require('../lang/'+lang);
+        resolve(fichier[label]);
+    })
+   
 }
